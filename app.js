2eed03b55867294e84420d6f1b7d0e6a03656d9d
Pawan Manghnani <84667321+TechnoAceX@users.noreply.github.com>
2024-06-21 22:29:30 +0530

    app.js

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
+// Retrieve the display element
 const display = document.getElementById("display");
 
+// Function to append input to the display
 function appendToDisplay(input) {
     display.value += input;
 }
 
+// Function to clear the display
 function clearDisplay() {
     display.value = "";
 }
 
+// Function to evaluate and display the result
 function calculate() {
-    try {
+    try{
         display.value = eval(display.value);
-    } catch (error) {
+    }
+    catch(error){
         display.value = "Error";
     }
 }
+
